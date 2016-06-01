b3caef1f4aa116f79830ecd80922a826f013d5b3
Adrian Clepcea <adi_clepcea@yahoo.com>
2016-06-01 22:22:23 +0300

    clean test.sh

--- server/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-unformatted_files=$(find . -name "*.go" | xargs gofmt -l)
-
-[ -z "$unformatted_files" ] && exit 0
-
-echo "\nUnformated files: \n###############\n$unformatted_files\n##############\nPlease run go fmt before commiting\n"
-
-exit 1
